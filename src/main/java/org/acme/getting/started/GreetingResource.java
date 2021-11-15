d4959d5fcb71f5fd266fa841f4522d9988ac5320
Zhen Wei <zhennweii94@gmail.com>
2021-11-15 20:34:46 +0800

    Implement ConfigMap

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.acme.getting.started;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,10 +10,14 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name="greeter.message", defaultValue = "default value")
+    String greetingMsg;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+//        return "Hello RESTEasy";
 
+        return greetingMsg;
     }
 }
